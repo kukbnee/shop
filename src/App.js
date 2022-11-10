eb7aa575cb2410c2cb94933b527b1e89bd24a6df
kukbni <kukbnee@gmail.com>
2022-11-10 10:52:27 +0900

    상품목록 출력

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Button, Navbar, Container, Nav, Row, Col} from 'react-bootstrap';
 import data from './data.js';
+
+
 
 function App() {
 
-  let [shoes] = useState(data);
-
+  let [shose] = useState(data);
+  
   return (
     <div className="App">
       
@@ -24,9 +26,30 @@ function App() {
       </Navbar>
 
       <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          {
+            shose.map(function(pData, idx) {
+              return(
+                <Product sendData={pData} i={idx+1}></Product>
+              )
+            })
+          }
+        </Row>
+      </Container>
       
     </div>
   );
 }
 
+function Product(props) {
+  return(
+      <Col>
+        <img src={props.sendData.imgPath} style={{height: "200px"}}></img>
+        <h4>{props.sendData.title}</h4>
+        <p>{props.sendData.price}</p>
+      </Col>
+  );
+}
 export default App;
--- src/data.js
+++ src/data.js
@@ -1,23 +1,28 @@
+
+
 let data = [
     {
       id : 0,
       title : "White and Black",
       content : "Born in France",
-      price : 120000
+      price : 120000,
+      imgPath : "gImg/dr_martens_001.jpg"
     },
   
     {
       id : 1,
       title : "Red Knit",
       content : "Born in Seoul",
-      price : 110000
+      price : 110000,
+      imgPath : "gImg/dr_martens_002.jpg"
     },
   
     {
       id : 2,
       title : "Grey Yordan",
       content : "Born in the States",
-      price : 130000
+      price : 130000,
+      imgPath : "gImg/dr_martens_003.jpg"
     }
   ];
 
